abd5f277f93e149dbfb6c8ca21b22ec9418cfda8
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-04 09:11:39 -0700

    condicionales

---
+++ condicionales/else.py
@@ -0,0 +1,12 @@
+if 3 < 2 :
+    print("3 es menor que 2")
+
+elif 3 == 2:    
+    print("3 es igual a 2")
+
+elif 3 > 3:    
+    print("3 es mayor o igual que 3" )
+
+else:
+    print("Ninguna de las condiciones anteriores se cumplió")
+
---
+++ condicionales/piedrapapelotijera.py
@@ -0,0 +1,16 @@
+t = 1
+p = 2
+pa = 3
+
+if t < pa:
+    print("Gana tijera")
+
+    if p > t:
+        print("Gana piedra")
+
+if pa > p:
+    print("Gana papel")
+
+if p < t:
+    print("Gana piedra")    
+
