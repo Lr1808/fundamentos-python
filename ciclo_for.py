2ac894bb7fa974ad1323bad44ad1a74e9a3de08a
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-04 11:21:49 -0700

    ciclo for

---
+++ ciclo_for.py
@@ -0,0 +1,17 @@
+frutas = ["manzana", "pera", "naranja"] 
+
+# for f in frutas:
+#     if f == "pera":
+#         print("He encontrado una pera!")
+#         print()
+#     break
+
+#for fruta in len(frutas):
+# print( len(frutas) )
+#for elemento in range(3):
+# print(elemento)
+
+for elemento in range(len(frutas)):
+    print(frutas[elemento])
+    print(elemento) 
+
--- estructuras_datos.py
+++ estructuras_datos.py
@@ -53,3 +53,58 @@ personas = {
 for iterar in fruits:
     fruits
     print:{fruits}
+
+# estructuras de datos python
+# enteros
+number = 10
+#print(type(10))
+
+# flotantes
+pi = 3.14
+#print(type(pi))
+
+# cadenas de texto
+greeting = "Hola, Mundo!"
+#print(type(greeting))
+
+# booleanos
+is_active = True
+#print(type(is_active))
+
+# listas
+fruits = [
+    "manzana", 
+    "banana", 
+    "cereza"
+    ]
+
+fruits.append("naranja")
+#print(fruits)
+
+
+#print(fruits[1:])
+
+#print(fruits)
+fruits[1] = "kiwi"
+#print(fruits)
+
+#tuplas
+nombres = (
+            "Ana",
+            "Luis", 
+            "Carlos", 
+            "Diana",
+            )
+#print (nombres)
+
+#diccionarios
+personas = {
+    "nombre": "Ana",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+
+#print(personas["nombre"])
+
+
+
