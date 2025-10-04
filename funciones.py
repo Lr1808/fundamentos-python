8e412c8ae54901ecd47bee4f0b88c278362d947a
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-04 12:10:42 -0700

    funciones

---
+++ funciones.py
@@ -0,0 +1,27 @@
+# #funciones en python
+
+# def saludar():
+#     print("Hola, Mundo!")
+
+
+# #saludos con parametros
+# def saludar_persona(nombre):
+#     print(f"Hola, {nombre}! Bienvenido a Python.")
+
+
+# #saludar_persona("Luis")
+
+# #saludos con parametros y retorno
+
+# def obtener_saludo(nombre):
+#     return f"Hola, {nombre}! Bienvenido a Python."
+
+# print(obtener_saludo("Ana"))
+
+# #saludos conmpleto
+
+# def obtener_saludo_completo(nombre="fulano", apellido="de tal"):
+#     return f"Hola, {nombre} {apellido}. Bienvenido a Python.
+
+# print(obtener_saludo_completo("Luis",))
+
